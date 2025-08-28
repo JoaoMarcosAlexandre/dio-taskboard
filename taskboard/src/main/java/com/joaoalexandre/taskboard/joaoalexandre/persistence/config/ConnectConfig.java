60e97cf8d71045976f099af4ddd44b6557afe5c7
JoaoAlexandre <alexandremjoao@gmail.com>
2025-08-28 12:46:18 -0300

    fix: "ConnectConfig.java"

--- taskboard/src/main/java/com/joaoalexandre/taskboard/joaoalexandre/persistence/config/ConnectConfig.java
+++ taskboard/src/main/java/com/joaoalexandre/taskboard/joaoalexandre/persistence/config/ConnectConfig.java
@@ -11,7 +11,7 @@ import static lombok.AccessLevel.PRIVATE;
 @AllArgsConstructor(access = PRIVATE)
 public final class ConnectConfig {
     public static Connection getConnection() throws SQLException {
-        var url = "jdbc:mysql://localhost:3306/board?useSSL=false&serverTimezone=UTC";
+        var url = "jdbc:mysql://localhost/board";
         var user = "board";
         var password = "board";
 
